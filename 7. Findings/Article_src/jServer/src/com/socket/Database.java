1f1eb17e0e1c0ca26bf3a392a98e97713c655544
AsrorWali <sardor-g@hotmail.com>
2015-10-06 13:41:00 +0200

    Chat finding
    chaterino

---
+++ 7. Findings/Article_src/jServer/src/com/socket/Database.java
@@ -0,0 +1,65 @@
+package com.socket;
+
+import java.io.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.*;
+
+public class Database {
+
+    public String filePath;
+
+    public Database(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public boolean userExists(String username) {
+
+        return true;
+    }
+
+    public boolean checkLogin(String username, String password) {
+
+        return true;
+    }
+
+    public void addUser(String username, String password) {
+
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(filePath);
+
+            Node data = doc.getFirstChild();
+
+            Element newuser = doc.createElement("user");
+            Element newusername = doc.createElement("username");
+            newusername.setTextContent(username);
+            Element newpassword = doc.createElement("password");
+            newpassword.setTextContent(password);
+
+            newuser.appendChild(newusername);
+            newuser.appendChild(newpassword);
+            data.appendChild(newuser);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filePath));
+            transformer.transform(source, result);
+
+        } catch (Exception ex) {
+            System.out.println("Exceptionmodify xml");
+        }
+    }
+
+    public static String getTagValue(String sTag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        return nValue.getNodeValue();
+    }
+}
